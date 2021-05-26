6f1e83e1b754012a476e006cf099c9c07c209e30
lisx03 <lisx03@163.com>
2021-05-26 13:59:34 +0800

    '日志器配置'

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,7 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+#1.导入系统logging
+import logging
+#2.创建日志器
+logger = logging.getLogger('django')
+
+from django.http import HttpResponse
+
+
+def log(request):
+    # 3.使用日志器
+    logging.info('info')
+    return HttpResponse('test')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',log),
 ]
